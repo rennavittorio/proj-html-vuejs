210091814b25d32dda25a56e42b606870b2ab4d8
rennavittorio <renna.vittorio@gmail.com>
2023-03-13 21:53:34 +0100

    DEL code refuse

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -37,24 +37,11 @@ export default {
 <template>
 
     <div class="card text-center">
-        <!-- <div class="img-wrapper group relative">
-            <img :src="cardImage" alt="">
-            <div class="img-overlay w-full h-full absolute top-0 left-0 hidden group-hover:flex group-hover:cursor-pointer flex-col justify-center items-center">
-
-                <div class="icon-wrapper w-[40px] h-[40px] p-1 bg-white text-orange rounded-full flex justify-center items-center mx-auto">
-                    <font-awesome-icon icon="fa-solid fa-link"/>
-                </div>
-                <div class="title__overlay p-3 text-2xl font-serif text-white">
-                    {{ cardTitle }}
-                </div>
-
-            </div>
-        </div> -->
         <AppCardImg 
         :cardImgImage="cardImage"
         :cardImgTitle="cardTitle"
         />
-        
+
         <h3 class="card__title p-3 text-2xl font-serif hover:text-orange hover:cursor-pointer">
             {{ cardTitle }}
         </h3>
@@ -67,13 +54,6 @@ export default {
 </template>
 
 <style scoped>
-/* .img-wrapper:hover .img-overlay {
-    background: linear-gradient(
-        to top,
-        rgba(255,76,0, 0.9) 35%,
-        rgba(255,76,0, 0.75) 75%,
-        rgba(255,76,0, 0.5) 100%
-    );
-} */
+
 
 </style>
