5642b7d31a95926bf6b6bb950f2a236f4dc31b0c
rennavittorio <renna.vittorio@gmail.com>
2023-03-14 17:00:23 +0100

    ADD graphic 4-main-articles into subscribe section

--- src/components/AppSubscribe.vue
+++ src/components/AppSubscribe.vue
@@ -1,15 +1,23 @@
 <script>
 import store from '../store';
 import AppBtn from './AppBtn.vue';
+import AppCardImg from './AppCardImg.vue';
 
 export default {
     components: {
         AppBtn,
+        AppCardImg,
     },
     data(){
         return {
             store,
         }
+    },
+    computed: {
+        topFourPopular(){
+            let filteredList = [...this.store.foodieJournal].slice(0,4);
+            return filteredList;
+        },
     }
 }
 
@@ -19,7 +27,7 @@ export default {
 
     <section class="subscribe-section h-[700px] flex justify-center items-center relative">
 
-        <div class="container bg-white py-5">
+        <div class="container bg-white py-10">
             <h1 class="section__title text-center font-serif text-2xl mb-5">
                 Subscribe <span class="text-orange">&</span> Receive A Free eBook
             </h1>
@@ -37,16 +45,26 @@ export default {
                 />
             </div>
 
-
-
         </div>
 
+    </section>
+
+    <section class="top-four-elements">
+        <div class="grid grid-cols-4">
+            <AppCardImg 
+            v-for="article in topFourPopular" :key="article.articleName"
+            :cardImgImage="article.articleCover"
+            :cardImgTitle="article.articleName"
+            />
+
+        </div>
     </section>
 
 </template>
 
 <style scoped>
 .subscribe-section {
-    background: url('/images/receipe-2.jpg');
+    background: url('/images/fi-organic-breakfast.jpg');
+    background-position: center;
 }
 </style>
