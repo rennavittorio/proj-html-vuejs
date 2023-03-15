e6d9cc6d3f1a3b124f54cd600b2abd39e6bd9cec
rennavittorio <renna.vittorio@gmail.com>
2023-03-15 12:39:01 +0100

    REFACTOR social bar hover

--- src/components/AppSocialBar.vue
+++ src/components/AppSocialBar.vue
@@ -46,8 +46,11 @@ export default {
 <template>
   <div class="social-action flex gap-x-1 -mx-2">
     <div v-for="social in socials" :key="social.socialName" 
-    class="social-icon text-lightgrey p-2 hover:text-orange hover:cursor-pointer">
+    class="social-icon text-lightgrey p-2 hover:text-grey hover:bg-white rounded-md hover:cursor-pointer relative group">
         <font-awesome-icon :icon="['fa-brands', social.iconName]" />
+        <div class="social__text bg-grey text-white hidden group-hover:block rounded-sm text-sm px-1 absolute bottom-[-50%] right-[50%] translate-x-[50%]">
+            {{ social.socialName }}
+        </div>
     </div>
   </div>
 </template>
